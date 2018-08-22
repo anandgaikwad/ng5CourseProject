912bf56411bf67ccaf17f02f24705e47e3ee9253
eastern_anandg <a.gaikwad@easternenterprise.com>
2018-08-22 19:37:12 +0530

    Added ngFor for todo list

--- my-fist-app/src/app/todo/todo.component.ts
+++ my-fist-app/src/app/todo/todo.component.ts
@@ -12,4 +12,17 @@ export class TodoComponent implements OnInit {
   ngOnInit() {
   }
 
+  todolist=[{Id:1,todo:'Create new components'},
+            {Id:2,todo:'Create example of property data binding'},
+            {Id:3,todo:'Create example of tow-way data binding'},
+            {Id:4,todo:'Create Child components'},
+            {Id:5,todo:'Link Child/multiple components'}
+          ];
+
+  selectedToDoItem:number=1;
+  onSelected(event,item)
+  {
+    this.selectedToDoItem=item.Id;
+  }
+
 }
